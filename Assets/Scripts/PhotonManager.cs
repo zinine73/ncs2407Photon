0fe77e9ac0248796a4ad7f4b2c97f76eb635fa02
zinine73 <zinine@naver.com>
2024-10-17 11:55:34 +0900

    photon connect

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    private readonly string version = "1.0";
+    private string userId = "zinine";
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = version;
+        PhotonNetwork.NickName = userId;
+
+        Debug.Log(PhotonNetwork.SendRate);
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"JoinRandom Failed {returnCode}:{message}");
+
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 20;
+        ro.IsOpen = true;
+        ro.IsVisible = true;
+
+        PhotonNetwork.CreateRoom("My Room", ro);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room");
+        Debug.Log($"Room name = {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}");
+        Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}");
+
+        foreach (var player in PhotonNetwork.CurrentRoom.Players)
+        {
+            Debug.Log($"{player.Value.NickName} , {player.Value.ActorNumber}");
+        }
+    }
+}
